932e76fc2c473efa606061d1c5dcde5dbb4e5850
floz <florian.zumbrunn@gmail.com>
2024-10-17 13:32:53 +0200

    updates

---
+++ src/mainMouse.js
@@ -0,0 +1,46 @@
+console.log( 'hello trigo' )
+
+const domApp = document.getElementById( 'app' )
+
+const width = window.innerWidth
+const height = window.innerHeight
+
+const canvas = document.createElement( 'canvas' )
+canvas.width = width
+canvas.height = height
+const ctx = canvas.getContext( '2d' )
+domApp.appendChild( canvas )
+
+//
+
+let xMouse = width * .5
+let yMouse = height * .5
+
+function onMouseMove( e ) {
+	xMouse = e.clientX
+	yMouse = e.clientY
+}
+document.body.addEventListener( 'mousemove', onMouseMove, false )
+
+const ball = {
+	x: width * .5,
+	y: height * .5,
+	radius: 20
+}
+const friction = .1
+
+function drawMouse() {
+	ctx.fillStyle = 'rgba( 255, 255, 255, .9 )'
+	ctx.fillRect( 0, 0, width, height )
+	
+	// TODO
+
+	ctx.beginPath()
+	ctx.fillStyle = 'red'
+	ctx.arc( ball.x, ball.y, ball.radius, 0, Math.PI * 2 )
+	ctx.fill()
+	ctx.closePath()
+
+	requestAnimationFrame( drawMouse )
+}
+drawMouse()
