23b8b8b5e35efcd6853be577ef4c678e150b53c4
floz <florian.zumbrunn@gmail.com>
2024-10-17 16:57:56 +0200

    updates

--- src/mainCircleBalls.js
+++ src/mainCircleBalls.js
@@ -13,7 +13,7 @@ domApp.appendChild( canvas )
 
 //
 
-const countBalls = 20
+const countBalls = 100
 let angle = 0
 const radius = 200
 
@@ -23,7 +23,7 @@ for( let i = 0; i < countBalls; i++ ) {
 	balls.push( { 
 		x: 0,
 		y: 0,
-		a: Math.random() * Math.PI * 2
+		a: a
 	} )
 	a += Math.PI * 2 / countBalls
 }
@@ -35,19 +35,19 @@ function drawTrigo() {
 	ctx.save()
 	ctx.translate( width * .5, height * .5 )
 
-	// ctx.beginPath()
-	// ctx.strokeStyle = 'black'
-	// ctx.arc( 0, 0, radius, 0, Math.PI * 2 )
-	// ctx.stroke()
-	// ctx.closePath()
+	ctx.beginPath()
+	ctx.strokeStyle = 'black'
+	ctx.arc( 0, 0, radius, 0, Math.PI * 2 )
+	ctx.stroke()
+	ctx.closePath()
 
 	ctx.save()
 
 	let a = 0
 	for( let i = 0; i < countBalls; i++ ) {
 		const ball = balls[ i ]
-		ball.x += Math.cos( ball.a ) * 1
-		ball.y += Math.sin( ball.a ) * 1
+		ball.x += Math.cos( ball.a ) * 6
+		ball.y += Math.sin( ball.a ) * 6
 
 		ctx.beginPath()
 		ctx.moveTo( 0, 0 )
